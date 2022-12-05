2f55f02598fe3b9ab4caf0e8aca3a3e66f10a7b4
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2022-12-05 16:10:46 +0900

    2022-12-05 : haha

---
+++ app/src/main/java/com/example/realtimem/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.realtimem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // haha
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
